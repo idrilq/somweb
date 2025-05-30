998647515db2aeb9c9f404b829234b24dbf537cc
idrilq <idrilq@gmail.com>
2025-05-30 12:02:06 +0500

    Create custom_appbar.dart

---
+++ my_fiskal_app/lib/shared/widgets/custom_appbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
