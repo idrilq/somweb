a2ede9535bfefd65addec6cf072359c9a1379118
idrilq <idrilq@gmail.com>
2025-05-30 12:00:37 +0500

    Create app_theme.dart

---
+++ my_fiskal_app/lib/core/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appLightTheme = ThemeData(
+  primarySwatch: Colors.green,
+  brightness: Brightness.light,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
+
+final ThemeData appDarkTheme = ThemeData(
+  primarySwatch: Colors.green,
+  brightness: Brightness.dark,
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+);
