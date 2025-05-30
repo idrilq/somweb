6cb9d59a95162d27a908d9ceb658cd3f1052842f
idrilq <idrilq@gmail.com>
2025-05-30 12:00:46 +0500

    Create registrator.dart

---
+++ my_fiskal_app/lib/data/models/registrator.dart
@@ -0,0 +1,21 @@
+class Registrator {
+  final String number;
+  final String name;
+  final String organization;
+  final String address;
+  final int fdCount;
+  final String filling;
+  final bool isBlocked;
+  final String state;
+
+  Registrator({
+    required this.number,
+    required this.name,
+    required this.organization,
+    required this.address,
+    required this.fdCount,
+    required this.filling,
+    required this.isBlocked,
+    required this.state,
+  });
+}
