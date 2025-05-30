795d189d912aca69e7956bc960611d34edde0724
idrilq <idrilq@gmail.com>
2025-05-30 12:00:19 +0500

    Create app_constants.dart

---
+++ my_fiskal_app/lib/core/app_constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppStrings {
+  static const appName = 'Fiscal Manager';
+  static const loginTitle = 'Авторизация';
+  static const addTerminal = 'Новый терминал';
+}
+
+class AppColors {
+  static const primary = Color(0xFF2E7D32);
+  static const secondary = Color(0xFF1565C0);
+  static const error = Color(0xFFD32F2F);
+}
+
+class AppDimens {
+  static const padding = 16.0;
+  static const borderRadius = 8.0;
+}
